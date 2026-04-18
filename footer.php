ef82e16bb8fafe15c16e0abbd710e1c3742eb165
nino <mekhrishvilinino02@gmail.com>
2026-04-18 17:35:29 +0400

    დავამატე header და footer ფაილები

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="footer_section">
+    <div class="container">
+      <p> &copy; <span id="displayYear"></span> All Rights Reserved By Free Html Templates </p>
+    </div>
+  </footer>
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>Mico Hospital</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <link href="css/style.css" rel="stylesheet" />
+  <link href="css/responsive.css" rel="stylesheet" />
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="hero_area">
+    <header class="header_section">
+      <div class="header_bottom">
+        <div class="container-fluid">
+          <nav class="navbar navbar-expand-lg custom_nav-container">
+            <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt=""></a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="treatment.php">Treatment</a></li>
+                <li class="nav-item"><a class="nav-link" href="doctor.php">Doctors</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.php">Contact Us</a></li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
